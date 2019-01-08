20510761538d41b580ba76ad56871f0ba64b575a
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2019-01-08 15:44:09 -0200

    Changes from mentor review

--- elixir/exercism.io/bob/bob.exs
+++ elixir/exercism.io/bob/bob.exs
@@ -3,21 +3,16 @@ defmodule Bob do
     is_yelling = input == String.upcase(input)
     is_a_question = String.last(input) == "?"
     is_empty = String.trim(input) == ""
-    has_numbers = Regex.run(~r/[\d]+/, input) != nil
-    has_alphabet_letters = Regex.run(~r/[a-zA-Z]+/, input) != nil
+    has_only_alphabet_letters = String.upcase(input) != String.downcase(input)
 
     cond do
-      is_a_question && not is_yelling ->
-        "Sure."
       is_empty ->
         "Fine. Be that way!"
-      is_yelling && is_a_question && not has_numbers ->
+      is_yelling && is_a_question && has_only_alphabet_letters ->
         "Calm down, I know what I'm doing!"
       is_a_question ->
         "Sure."
-      has_numbers && not has_alphabet_letters ->
-        "Whatever."
-      is_yelling ->
+      is_yelling && has_only_alphabet_letters ->
         "Whoa, chill out!"
       true ->
         "Whatever."
