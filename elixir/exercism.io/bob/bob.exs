12e4182a329c4ebf16a41fa309dadb9b4fb2f4fe
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2019-01-09 13:13:08 -0200

    Use private functions on bob exercise solution

--- elixir/exercism.io/bob/bob.exs
+++ elixir/exercism.io/bob/bob.exs
@@ -1,21 +1,21 @@
 defmodule Bob do
   def hey(input) do
-    is_yelling = input == String.upcase(input)
-    is_a_question = String.last(input) == "?"
-    is_empty = String.trim(input) == ""
-    has_only_alphabet_letters = String.upcase(input) != String.downcase(input)
-
     cond do
-      is_empty ->
+      is_empty?(input) ->
         "Fine. Be that way!"
-      is_yelling && is_a_question && has_only_alphabet_letters ->
+      is_yelling?(input) && is_a_question?(input) && has_only_alphabet_letters?(input) ->
         "Calm down, I know what I'm doing!"
-      is_a_question ->
+      is_a_question?(input) ->
         "Sure."
-      is_yelling && has_only_alphabet_letters ->
+      is_yelling?(input) && has_only_alphabet_letters?(input) ->
         "Whoa, chill out!"
       true ->
         "Whatever."
     end
   end
+
+  defp is_yelling?(input), do: input == String.upcase(input)
+  defp is_a_question?(input), do: String.last(input) == "?"
+  defp is_empty?(input), do: String.trim(input) == ""
+  defp has_only_alphabet_letters?(input), do: String.upcase(input) != String.downcase(input)
 end
